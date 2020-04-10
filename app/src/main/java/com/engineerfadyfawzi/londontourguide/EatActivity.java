ce8646ba509f20ba41ddef4eb449524b97df2285
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-04-10 13:45:24 +0200

    1.2 Create New Activities for each Category

---
+++ app/src/main/java/com/engineerfadyfawzi/londontourguide/EatActivity.java
@@ -0,0 +1,16 @@
+package com.engineerfadyfawzi.londontourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EatActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_eat );
+    }
+}
---
+++ app/src/main/java/com/engineerfadyfawzi/londontourguide/TodoActivity.java
@@ -0,0 +1,16 @@
+package com.engineerfadyfawzi.londontourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_todo );
+    }
+}
