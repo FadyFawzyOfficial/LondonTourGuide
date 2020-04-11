0e1691aa0c11578cdfce0f2974dd5f66482a7c92
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-04-11 17:50:08 +0200

    1.11 Create ArrayList of Places in StayActivity (with fake data to test) and Connect it with PlaceAdapter

--- app/src/main/java/com/engineerfadyfawzi/londontourguide/StayActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/londontourguide/StayActivity.java
@@ -3,6 +3,9 @@ package com.engineerfadyfawzi.londontourguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class StayActivity extends AppCompatActivity
 {
@@ -11,6 +14,28 @@ public class StayActivity extends AppCompatActivity
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_stay );
+        setContentView( R.layout.place_list );
+        
+        // Create an ArrayList of Place objects
+        ArrayList< Place > places = new ArrayList< Place >();
+        places.add( new Place( R.mipmap.test, R.string.stay_name_one, R.string.stay_location_one,
+                R.string.stay_website_one, R.string.stay_phone_one ) );
+        places.add( new Place( R.mipmap.test, R.string.stay_name_one, R.string.stay_location_one,
+                R.string.stay_website_one, R.string.stay_phone_one ) );
+        places.add( new Place( R.mipmap.test, R.string.stay_name_one, R.string.stay_location_one,
+                R.string.stay_website_one, R.string.stay_phone_one ) );
+        places.add( new Place( R.mipmap.test, R.string.stay_name_one, R.string.stay_location_one,
+                R.string.stay_website_one, R.string.stay_phone_one ) );
+        places.add( new Place( R.mipmap.test, R.string.stay_name_one, R.string.stay_location_one,
+                R.string.stay_website_one, R.string.stay_phone_one ) );
+        
+        // Create an {@link PlaceAdapter}, whose data source is a list of
+        // {@link Place}s. The adapter knows how to create list item views
+        // for each item in the list.
+        PlaceAdapter placeAdapter = new PlaceAdapter( this, places );
+        
+        // Get a reference to the ListView, and attach the adapter to the listView.
+        ListView listView = findViewById( R.id.list_view );
+        listView.setAdapter( placeAdapter );
     }
 }
