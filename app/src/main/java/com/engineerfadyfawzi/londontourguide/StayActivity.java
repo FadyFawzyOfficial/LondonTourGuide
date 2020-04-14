6e1b9da52e752ddda1c946331bd8a06ba9e694ce
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-04-14 13:44:03 +0200

    1.14 Add Click Listener on list item to open website URL (in StayActivity)

--- app/src/main/java/com/engineerfadyfawzi/londontourguide/StayActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/londontourguide/StayActivity.java
@@ -2,7 +2,11 @@ package com.engineerfadyfawzi.londontourguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -32,10 +36,31 @@ public class StayActivity extends AppCompatActivity
         // Create an {@link PlaceAdapter}, whose data source is a list of
         // {@link Place}s. The adapter knows how to create list item views
         // for each item in the list.
-        PlaceAdapter placeAdapter = new PlaceAdapter( this, places, R.color.category_stay );
+        final PlaceAdapter placeAdapter = new PlaceAdapter( this, places, R.color.category_stay );
         
         // Get a reference to the ListView, and attach the adapter to the listView.
         ListView listView = findViewById( R.id.list_view );
         listView.setAdapter( placeAdapter );
+        
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website which is owned by the selected place.
+        listView.setOnItemClickListener( new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick( AdapterView< ? > adapterView, View view, int position, long l )
+            {
+                // Find the current place that was clicked
+                Place currentPlace = placeAdapter.getItem( position );
+                
+                // Convert the String URL (website) into a URI object (to pass into the Intent constructor)
+                Uri placeUri = Uri.parse( getString( currentPlace.getPlaceWebsiteId() ) );
+                
+                // Create a new intent to view the place URI
+                Intent websiteIntent = new Intent( Intent.ACTION_VIEW, placeUri );
+                
+                // Send the intent to launch a new activity
+                startActivity( websiteIntent );
+            }
+        } );
     }
 }
